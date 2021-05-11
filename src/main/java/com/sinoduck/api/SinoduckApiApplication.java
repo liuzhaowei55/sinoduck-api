002167c857bfcfa74995da2a9251ac90bb544bb9
where.liu <where.liu@huolala.cn>
2021-05-11 09:33:24 +0800

    feat: save

---
+++ src/main/java/com/sinoduck/api/SinoduckApiApplication.java
@@ -0,0 +1,13 @@
+package com.sinoduck.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinoduckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinoduckApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinoduck/api/SinoduckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinoduck.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinoduckApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
