7cc4b887a9dad4bb2185fd202fadca1690ea8bf0
where.liu <where.liu@huolala.cn>
2021-05-21 09:21:09 +0800

    feat: save

--- src/main/java/com/sinoduck/api/dao/domain/UserDo.java
+++ src/main/java/com/sinoduck/api/dao/domain/UserDo.java
@@ -1,5 +1,6 @@
 package com.sinoduck.api.dao.domain;
 
+import cn.hutool.crypto.digest.BCrypt;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -11,5 +12,12 @@ import lombok.EqualsAndHashCode;
 @Data
 @TableName(value = "user")
 public class UserDo extends BaseDomain {
+    private String username;
+    private String password;
+    private String email;
+    private String avatar;
 
+    public void setPassword(String password) {
+        this.password = BCrypt.hashpw(password);
+    }
 }
